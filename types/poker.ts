b2adb15800ad3ca6e90342c81b314e95f4cb3d6b
Joshua Cancio <jd.cancio1@gmail.com>
2026-04-23 14:03:39 -0400

    added session_id definition

--- types/poker.ts
+++ types/poker.ts
@@ -2,6 +2,7 @@ export interface Player {
   id: string;
   name: string;
   buy_in: number;
+  session_id: string;
   chips: number; // Final chip count at the end of the night
   result?: number;
 }
